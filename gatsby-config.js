10c91258f3ef556472398a7f66f9251422b9a5f0
tyjch <tyjchurchill@gmail.com>
2022-06-17 16:01:12 -0700

    Adding action to deploy to pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/
+        }
+      }
+    }
+  ],
 }
