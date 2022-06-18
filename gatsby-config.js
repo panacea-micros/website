5bf9de84bde1e0646797a089dd35f45d5e958744
tyjch <tyjchurchill@gmail.com>
2022-06-17 21:12:55 -0700

    Added favicons, manifest, 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,24 @@ module.exports = {
           include: /assets/
         }
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#253f21",
+        theme_color: "#253f21",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.svg", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
